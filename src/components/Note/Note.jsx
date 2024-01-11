5c1ed6f1f2d806ad8303c4c6a96cbd46e21da1d6
NickmDrummer <nickmdrummer@gmail.com>
2024-01-11 09:54:26 -0300

    Ajustando el texto a la nota otra vez

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -7,7 +7,7 @@ function Note() {
       <img src="./img/note/note.png" style={{ width: '100%' }} alt="Note" />
 
       <div className="note-content">
-        <h2>{titulo}</h2>
+        <h3>{titulo}</h3>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eos enim
           deserunt consequatur iure explicabo, provident eveniet odio ea quasi
@@ -15,7 +15,13 @@ function Note() {
           ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus
           quibusdam voluptatum, consequatur minima nemo voluptates suscipit
           excepturi provident ipsum, ullam rerum? Dolor, eius aliquid! Cumque
-          dolor nostrum voluptate minima adipisci.
+          dolor nostrum voluptate minima adipisci. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Ratione inventore eveniet esse sed,
+          natus delectus veniam a qui autem ipsam nostrum hic est corporis et
+          fuga labore tenetur saepe obcaecati. Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Placeat consequatur a explicabo,
+          deserunt sunt voluptate illo non, quis voluptatum ipsa, aspernatur
+          pariatur? Nobis est illo incidunt temporibus praesentium fuga cum?
         </p>
       </div>
     </div>
