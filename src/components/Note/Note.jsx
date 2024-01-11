9c7717a60f7bddda5e52f3955118bb6ea76e88eb
NickmDrummer <nickmdrummer@gmail.com>
2024-01-11 16:38:16 -0300

    Agregando iconos a la nota

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,8 +1,13 @@
+import close from './../../../public/img/close-circle-sharp.svg';
+import pen from './../../../public/img/pencil-sharp.svg';
+
 import './Note.css';
 function Note({ titulo, contenido }) {
   return (
     <div className="note">
       <img src="./img/note/note.png" style={{ width: '100%' }} alt="Note" />
+      <img id="pen" src={pen} className="notaIconos" alt="Editar" />
+      <img id="close" src={close} className="notaIconos" alt="Borrar" />
 
       <div className="note-content">
         <h3>{titulo}</h3>
