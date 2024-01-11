6d165467bb42c8da4fa95032336f1afd21b9c2f1
NickmDrummer <nickmdrummer@gmail.com>
2024-01-11 10:21:59 -0300

    Añadiendo Bootstrap al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import Note from './components/Note/Note';
 function App() {
   return (
     <div className="App">
+      <div className="nuevaNota">
+        <button
+          type="button"
+          className="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          data-bs-whatever="@mdo"
+        >
+          Open modal for @mdo
+        </button>
+      </div>
       <div className="notasContainer">
         <Note />
         <Note />
